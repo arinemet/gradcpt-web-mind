f5b0dc6e3dc19504df915915678d54115e152c23
Ari <ari.m.nemet@gmail.com>
2026-07-18 16:21:33 -0700

    Add psychojs into node_modules and add import + type declerations
    Signed-off-by: Ari <ari.m.nemet@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import * as psychojs from "psychojs";
+
 const canvas = document.querySelector("canvas")!;
 const ctx = canvas.getContext("2d")!;
 const rtimeDiv = document.querySelector("#rtime")!;
