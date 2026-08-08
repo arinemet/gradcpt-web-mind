ee10c2468acfeefad722dbbf5948b78af6bbaa5a
Ari <ari.m.nemet@gmail.com>
2026-08-08 15:23:44 -0700

    Start a little refactor [rebase this into another bigger commit]

--- python/am.py
+++ python/am.py
@@ -1,5 +1,5 @@
-from scipy.io import wavfile
 import numpy as np
+from scipy.io import wavfile
 
 fs, data = wavfile.read("./public/Adele_Hello.wav")
 
@@ -9,7 +9,7 @@ num_samples = data.shape[0]
 
 t = np.arange(num_samples) / fs
 
-carrier_freq = 2 
+carrier_freq = 2
 modulation_index = 0.8
 
 carrier = 1 + modulation_index * np.cos(2 * np.pi * carrier_freq * t)
@@ -19,6 +19,7 @@ if data.ndim == 2:
 
 modulated = data * carrier
 
+# normalization (so that none of the samples get clipped for being more than max volume)
 modulated = modulated / np.max(np.abs(modulated))
 modulated = (32767 * modulated).astype(np.int16)
 
--- src/audio.ts
+++ src/audio.ts
@@ -1,37 +1,50 @@
-export async function loadAudio(
+export async function amplitudeModulation(
   fileName: string,
-  carrierFreq = 2,
-  modulationIndex = 0.8,
+  carrierFreq: number,
+  modulationIndex: number,
 ) {
   const audioCtx = new AudioContext();
   const response = await fetch(fileName);
   const data = await response.arrayBuffer();
   const buffer = await audioCtx.decodeAudioData(data);
 
+  const numSamples = buffer.length;
+  // sample rate of the audio
+  const sr = buffer.sampleRate;
+  let max = 0;
+
+  for (let i = 0; i < numSamples; i++) {
+    const t = i / sr;
+    const carrier =
+      1 + modulationIndex * Math.cos(2 * Math.PI * carrierFreq * t);
+
+    for (let channel = 0; channel < buffer.numberOfChannels; channel++) {
+      const samples = buffer.getChannelData(channel);
+      samples[i] *= carrier;
+      max = Math.max(max, Math.abs(samples[i]));
+    }
+  }
+
+  if (max > 0) {
+    for (let channel = 0; channel < buffer.numberOfChannels; channel++) {
+      const samples = buffer.getChannelData(channel);
+      for (let i = 0; i < numSamples; i++) {
+        samples[i] /= max;
+      }
+    }
+  }
+
   const source = audioCtx.createBufferSource();
-  const gain = audioCtx.createGain();
-  const carrier = audioCtx.createOscillator();
-  const carrierGain = audioCtx.createGain();
-
   source.buffer = buffer;
-  gain.gain.value = 1;
-  carrier.frequency.value = carrierFreq;
-  carrierGain.gain.value = modulationIndex;
-
-  carrier.connect(carrierGain);
-  carrierGain.connect(gain.gain);
-  source.connect(gain);
-  gain.connect(audioCtx.destination);
+  source.connect(audioCtx.destination);
 
   return {
     start() {
       audioCtx.resume();
-      carrier.start();
       source.start();
     },
     stop() {
       source.stop();
-      carrier.stop();
       audioCtx.close();
     },
   };
