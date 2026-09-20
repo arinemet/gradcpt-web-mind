62352a83e7840d7c48a71ab5498180de86482f2f
Ari <ari.m.nemet@gmail.com>
2026-09-19 21:42:03 -0700

    Redundant space before bolded text
    Signed-off-by: Ari <ari.m.nemet@gmail.com>

--- src/gradcpt-instructions.ts
+++ src/gradcpt-instructions.ts
@@ -13,7 +13,7 @@ export class GradCptInstructionsPlugin {
   }
 
   trial(displayElement: HTMLElement) {
-    displayElement.innerHTML = `<div class="gradcpt-instructions"><p>Now you will perform the attention task. A series of 10 pictures will gradually be presented on the screen one after another. <em>In this phase of the experiment, you will press SPACEBAR when you see a picture of a city scene, and you will NOT press SPACEBAR when you see a picture of a mountain scene.</em> <b> Click on the text to continue.</b></p></div>`;
+    displayElement.innerHTML = `<div class="gradcpt-instructions"><p>Now you will perform the attention task. A series of 10 pictures will gradually be presented on the screen one after another. <em>In this phase of the experiment, you will press SPACEBAR when you see a picture of a city scene, and you will NOT press SPACEBAR when you see a picture of a mountain scene.</em> <b>Click on the text to continue.</b></p></div>`;
 
     displayElement.addEventListener(
       "click",
